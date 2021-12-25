8314659c0083c787273614ec6071a66447fc5a64
g-eipi10 <github_hobby@ntnguyen.anonaddy.com>
2021-12-25 17:39:08 +0700

    update

--- Windows Auto Build.cmd
+++ Windows Auto Build.cmd
@@ -1,4 +1,6 @@
 @ECHO OFF
+IF NOT EXIST "%~DP07za.exe" GOTO :Exit
+IF NOT EXIST "%~DP07za.dll" GOTO :Exit
 :Code
 ECHO Auto build script for extension ^<Download with Aria2^>
 ECHO 1. Chromium
@@ -8,9 +10,11 @@ IF %Option% EQU 1 SET Code=Chromium
 IF %Option% EQU 2 SET Code=Firefox
 IF NOT DEFINED Code CLS && GOTO :Code
 FOR /F "USEBACKQ SKIP=3 TOKENS=1,2 DELIMS=,: " %%I IN ("%~DP0%Code%\manifest.json") DO (IF %%~I EQU version SET Version=%%~J)
-%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -Command "Compress-Archive -Path '%~DP0Chromium\*' -DestinationPath '%~DP0%Code%-%Version%.zip'"
-IF %Option% EQU 2 %SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -Command "Compress-Archive -Path '%~DP0Firefox\*' -Update -DestinationPath '%~DP0%Code%-%Version%.zip'"
-ECHO Download with Aria2 for %Code% version %Version% build completed...
+"%~DP07za.exe" a "%~DP0%Code%-%Version%.zip" "%~DP0Chromium\*"
+IF %Option% EQU 2 "%~DP07za.exe" u "%~DP0%Code%-%Version%.zip" "%~DP0Firefox\*"
+ECHO.
+ECHO.
+ECHO %Code% %Version% build completed, script will be terminated in 5 seconds...
 :Exit
 TIMEOUT /T 5
 EXIT
